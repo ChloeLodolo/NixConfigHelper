b65c4a1552aba62e613ad4991f1a6fa436c8e71c
ChloeLodolo <32088560+ChloeLodolo@users.noreply.github.com>
2020-04-09 18:34:19 +0200

    Checkboxes, search button and a toast

--- app/src/main/java/couclou/fr/nixconfighelper/MainActivity.kt
+++ app/src/main/java/couclou/fr/nixconfighelper/MainActivity.kt
@@ -2,11 +2,19 @@ package couclou.fr.nixconfighelper
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val searchButton = findViewById<Button>(R.id.searchButton)
+
+        searchButton.setOnClickListener{
+            Toast.makeText(this, "No corresponding resource found in database", Toast.LENGTH_LONG).show()
+        }
     }
 }
